8a959d85cca646acef63d56d5785c1b00844b9f7
Batranovic <batranovic.ra48.2020@uns.ac.rs>
2024-10-15 10:57:11 +0200

    initial login/logout feature

--- Konteh/back-office-app/src/app/app.component.ts
+++ Konteh/back-office-app/src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MsalService } from '@azure/msal-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'back-office-app';
+
+  constructor(private msalService: MsalService) { }
+
+  ngOnInit(): void {
+    this.msalService.instance.initialize().then(() => {
+      this.msalService.instance.handleRedirectPromise().then(
+        res => {
+          if (res != null && res.account != null) {
+            this.msalService.instance.setActiveAccount(res.account);
+          }
+        }
+      )
+    })
+  }
+
+  isLoggedIn(): boolean {
+    return this.msalService.instance.getActiveAccount() != null;
+  }
+
+  login() {
+    this.msalService.loginRedirect();
+  }
+
+  logout() {
+    this.msalService.logout();
+  }
 }
--- Konteh/back-office-app/src/app/app.module.ts
+++ Konteh/back-office-app/src/app/app.module.ts
@@ -4,6 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MSAL_INSTANCE, MsalModule, MsalService } from '@azure/msal-angular'
+import { IPublicClientApplication, PublicClientApplication } from '@azure/msal-browser';
+
+export function MSALInstanceFactory(): IPublicClientApplication {
+  return new PublicClientApplication({
+    auth: {
+      clientId: '4e1ff54b-bf34-4f45-83ce-e50fc32967cd',
+      authority: 'https://login.microsoftonline.com/common',
+      redirectUri: 'http://localhost:4200',
+    }
+  })
+}
 
 @NgModule({
   declarations: [
@@ -11,10 +23,15 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MsalModule
   ],
   providers: [
-    provideAnimationsAsync()
+  {
+    provide: MSAL_INSTANCE,
+    useFactory: MSALInstanceFactory
+  },
+  MsalService
   ],
   bootstrap: [AppComponent]
 })
