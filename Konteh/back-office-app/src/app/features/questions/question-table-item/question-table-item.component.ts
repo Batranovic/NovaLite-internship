d5f78d8b773a8d9118906dea16a11f981c5240d6
OgnjenGligoric <gligoric383@gmail.com>
2024-10-15 11:30:16 +0200

    feat: ui table and paginator

---
+++ Konteh/back-office-app/src/app/features/questions/question-table-item/question-table-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-question-table-item',
+  templateUrl: './question-table-item.component.html',
+  styleUrl: './question-table-item.component.css'
+})
+export class QuestionTableItemComponent {
+
+}
--- Konteh/back-office-app/src/app/features/questions/questions.module.ts
+++ Konteh/back-office-app/src/app/features/questions/questions.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { QuestionsOverviewComponent } from './questions-overview/questions-overview.component';
+import { QuestionTableItemComponent } from './question-table-item/question-table-item.component';
+import {MatCell, MatColumnDef, MatHeaderCell, MatHeaderRow, MatRow, MatTable} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
 
 
 
 @NgModule({
   declarations: [
-    QuestionsOverviewComponent
+    QuestionsOverviewComponent,
+    QuestionTableItemComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatTable,
+    MatColumnDef,
+    MatHeaderCell,
+    MatCell,
+    MatHeaderRow,
+    MatRow,
+    MatPaginator
   ]
 })
 export class QuestionsModule { }
