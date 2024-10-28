85531120aa1b44974a2a9ed4fba1d2a4a4e201d6
Tamara Miljevic <116558563+miljevictamara@users.noreply.github.com>
2024-10-28 14:31:17 +0100

    fix: changes in GetAllExams

--- Konteh/Konteh.BackOfficeApi/Features/Exams/GetAllExams.cs
+++ Konteh/Konteh.BackOfficeApi/Features/Exams/GetAllExams.cs
@@ -32,6 +32,14 @@ public static class GetAllExams
 
         public async Task<IEnumerable<Response>> Handle(Query request, CancellationToken cancellationToken)
         {
+            var searchTerms = request.Candidate?.Trim().Split(' ') ?? [];
+            if (searchTerms.Length == 2)
+            {
+                var firstName = searchTerms[0];
+                var lastName = searchTerms[1];
+                var searchedExams = await _repository.Search(e => string.IsNullOrEmpty(request.Candidate) || (e.Candiate.Name.Contains(firstName) && e.Candiate.Surname.Contains(lastName)));
+                return searchedExams.Select(e => new Response { Id = e.Id, Candidate = $"{e.Candiate.Name} {e.Candiate.Surname}", Status = e.Status, Score = CalculateScore(e) });
+            }
             var exams = await _repository.Search(e => string.IsNullOrEmpty(request.Candidate) || e.Candiate.Name.Contains(request.Candidate) || e.Candiate.Surname.Contains(request.Candidate));
             return exams.Select(e => new Response { Id = e.Id, Candidate = $"{e.Candiate.Name} {e.Candiate.Surname}", Status = e.Status, Score = CalculateScore(e) });
         }
