609233f1c9391bfa9c8a6372f4ce052823e46895
Tamara Miljevic <116558563+miljevictamara@users.noreply.github.com>
2024-10-30 14:01:40 +0100

    fix: resolving merge conflicts

--- Konteh/Konteh.BackOfficeApi/Features/Exams/GetAllExams.cs
+++ Konteh/Konteh.BackOfficeApi/Features/Exams/GetAllExams.cs
@@ -12,16 +12,8 @@ public static class GetAllExams
         public string? Candidate { get; set; } = string.Empty;
     }
 
-    public class Response
-    {
-        public long Id { get; set; }
-        public string Candidate { get; set; } = string.Empty!;
-        public ExamStatus Status { get; set; }
-        public int Score { get; set; }
-        public int MaxScore { get; set; }
-    }
 
-    public class RequestHandler : IRequestHandler<Query, IEnumerable<Response>>
+    public class RequestHandler : IRequestHandler<Query, IEnumerable<GetExamResponse>>
     {
         private readonly IRepository<Exam> _examRepository;
 
@@ -37,13 +29,12 @@ public static class GetAllExams
             || e.Candiate.Name.Contains(request.Candidate)
             || e.Candiate.Surname.Contains(request.Candidate));
 
-            return exams.Select(e => new Response
+            return exams.Select(e => new GetExamResponse
             {
                 Id = e.Id,
                 Candidate = $"{e.Candiate.Name} {e.Candiate.Surname}",
                 Status = e.Status,
                 Score = e.ExamQuestions.Count(ea => ea.IsCorrect()),
-                MaxScore = e.ExamQuestions.Count
             });
         }
     }
